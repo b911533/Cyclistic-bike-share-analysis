892aa9e9a915157da7b09d57da9318f7debb2936
b911533 <77670255+b911533@users.noreply.github.com>
2022-09-21 02:01:46 -0500

    Update Cyclist_Analysis.R

--- Analysis/Cyclist_Analysis.R
+++ Analysis/Cyclist_Analysis.R
@@ -8,16 +8,16 @@ getwd() #displays working directory
 # STEP 1: COLLECT DATA
 #=====================
 # Upload Divvy datasets (csv files) here
-m6_2022 <- read_csv("/Users/jimchan/googleDAcertificate/case1/data/202206-divvy-tripdata.csv")
-m7_2022 <- read_csv("/Users/jimchan/googleDAcertificate/case1/data/202207-divvy-tripdata.csv")
-m8_2022 <- read_csv("/Users/jimchan/googleDAcertificate/case1/data/202208-divvy-tripdata.csv")
-m5_2022 <- read_csv("/Users/jimchan/googleDAcertificate/case1/data/202205-divvy-tripdata.csv")
+m6_2022 <- read_csv("/Users/.../202206-divvy-tripdata.csv")
+m7_2022 <- read_csv("/Users/.../202207-divvy-tripdata.csv")
+m8_2022 <- read_csv("/Users/.../202208-divvy-tripdata.csv")
+m5_2022 <- read_csv("/Users/.../202205-divvy-tripdata.csv")
 
 #====================================================
 # STEP 2: WRANGLE DATA AND COMBINE INTO A SINGLE FILE
 #====================================================
 # Compare column names each of the files
-# While the names don't have to be in the same order, they DO need to match perfectly before we can use a command to join them into one file
+# arrange column names in the same order to join
 colnames(m7_2022)
 colnames(m8_2022)
 colnames(m6_2022)
@@ -49,27 +49,17 @@ all_trips <- all_trips %>%
 #======================================================
 # Inspect the new table that has been created
 colnames(all_trips)  #List of column names
-nrow(all_trips)  #How many rows are in data frame?
-dim(all_trips)  #Dimensions of the data frame?
-head(all_trips)  #See the first 6 rows of data frame.  Also tail(all_trips)
-str(all_trips)  #See list of columns and data types (numeric, character, etc)
-summary(all_trips)  #Statistical summary of data. Mainly for numerics
+nrow(all_trips)  #rows are in data frame
+dim(all_trips)  #Dimensions of the data frame
+head(all_trips)  
+str(all_trips)  
+summary(all_trips)  #Statistical summary of data
 
-# There are a few problems we will need to fix:
-# (1) In the "member_casual" column, there are two names for members ("member" and "Subscriber") and two names for casual riders ("Customer" and "casual"). We will need to consolidate that from four to two labels.
-# (2) The data can only be aggregated at the ride-level, which is too granular. We will want to add some additional columns of data -- such as day, month, year -- that provide additional opportunities to aggregate the data.
-# (3) We will want to add a calculated field for length of ride since the 2020Q1 data did not have the "tripduration" column. We will add "ride_length" to the entire dataframe for consistency.
-# (4) There are some rides where tripduration shows up as negative, including several hundred rides where Divvy took bikes out of circulation for Quality Control reasons. We will want to delete these rides.
-
-# In the "member_casual" column, replace "Subscriber" with "member" and "Customer" with "casual"
-# Before 2020, Divvy used different labels for these two types of riders ... we will want to make our dataframe consistent with their current nomenclature
-# N.B.: "Level" is a special property of a column that is retained even if a subset does not contain any values from a specific level
-# how many observations fall under each usertype
+# observations fall under each usertype
 table(all_trips$member_casual)
 
 # Add columns that list the date, month, day, and year of each ride
-# Aggregate ride data for each month, day, or year ... before completing these operations we could only aggregate at the ride level
-# https://www.statmethods.net/input/dates.html more on date formats in R found at that link
+# Aggregate ride data 
 all_trips$date <- as.Date(all_trips$started_at) #The default format is yyyy-mm-dd
 all_trips$month <- format(as.Date(all_trips$date), "%m")
 all_trips$day <- format(as.Date(all_trips$date), "%d")
@@ -83,15 +73,12 @@ all_trips$ride_length <- difftime(all_trips$ended_at,all_trips$started_at)
 # Inspect the structure of the columns
 str(all_trips)
 
-# Convert "ride_length" from Factor to numeric so we can run calculations on the data
+# Convert "ride_length" from Factor to numeric in order to run calculations on the data
 is.factor(all_trips$ride_length)
 all_trips$ride_length <- as.numeric(as.character(all_trips$ride_length))
 is.numeric(all_trips$ride_length)
 
-# Removing unusual data
-# The dataframe includes a few hundred entries when bikes were taken out of docks and checked for quality by Divvy or ride_length was negative
-# We will create a new version of the dataframe (v2) since data is being removed
-# https://www.datasciencemadesimple.com/delete-or-drop-rows-in-r-with-conditions-2/
+# Removing unusual data : ride_length that are negative
 all_trips_v2 <- all_trips[-all_trips$ride_length<0,]
 
 #=====================================
@@ -112,10 +99,10 @@ aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = median)
 aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = max)
 aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = min)
 
-# See the average ride time by each day for members vs casual users
+# comparing the average ride time by each day for members vs casual users
 aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
 
-# Notice that the days of the week are out of order. Let's fix that.
+# arranging the days of the week in order
 all_trips_v2$day_of_week <- ordered(all_trips_v2$day_of_week, levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
 
 # computing average ride time by each day for members vs casual users
@@ -153,9 +140,5 @@ all_trips_v2 %>%
 # STEP 5: EXPORT SUMMARY FILE FOR FURTHER ANALYSIS
 #=================================================
 # Create a csv file that we will visualize in Excel, Tableau, or my presentation software
-# N.B.: This file location is for a Mac. If you are working on a PC, change the file location accordingly (most likely "C:\Users\YOUR_USERNAME\Desktop\...") to export the data. You can read more here: https://datatofish.com/export-dataframe-to-csv-in-r/
 counts <- aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
 write.csv(counts, file = '/Users/jimchan/googleDAcertificate/case1/data/avg_ride_length.csv')
-
-#You're done! Congratulations!
-
